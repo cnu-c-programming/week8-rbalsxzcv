3ddf8cbcaa0f023c27fba8a7124e18bf8fcd97cb
rbalsxzcv <150316157+rbalsxzcv@users.noreply.github.com>
2026-04-26 13:36:31 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -6,19 +6,19 @@ int main() {
     char *names[] = {"apple", "mango","ant","banana","cat","anaconda"};
     int n = sizeof(names) / sizeof(names[0]);
 
-    // int i = 0, count = 0;
-    // while (count < (n-1)*(n-1))
-    // {
-    //     if(strcmp(*(names+i),*(names+i+1))==1){
-    //         char* tmp = *(names+i);
-    //         *(names+i) = *(names+i+1);
-    //         *(names+i+1) = tmp;
-    //     }
-    //     if(i+2 == n){
-    //         i = 0;
-    //     }else{ i++;}
-    //     count++;
-    // }
+    int i = 0, count = 0;
+    while (count < (n-1)*(n-1))
+    {
+        if(strcmp(*(names+i),*(names+i+1))==1){
+            char* tmp = *(names+i);
+            *(names+i) = *(names+i+1);
+            *(names+i+1) = tmp;
+        }
+        if(i+2 == n){
+            i = 0;
+        }else{ i++;}
+        count++;
+    }
     
 
     for(int i = 0;i<n;i++){
