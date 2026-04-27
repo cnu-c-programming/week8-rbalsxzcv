ac204072dea07143c99e8134c01f4720611d4bf6
rbalsxzcv <150316157+rbalsxzcv@users.noreply.github.com>
2026-04-27 17:07:42 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
     char* tmp;
     for(int i = 0;i<argc-1;i++){
         for(int j = 0;j<argc-1-i;j++){
-            if(strcmp(*(argv+j),*(argv+j+1))==1){
+            if(strcmp(*(argv+j),*(argv+j+1))>=1){
                 tmp = *(argv+j);
                 *(argv+j) = *(argv+j+1);
                 *(argv+j+1) = tmp;
